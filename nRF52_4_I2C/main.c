93b47363f8faa121fc01642aa95c9da4105d77cb
Jihwan Kim <raptorwoo@gmail.com>
2022-07-20 19:02:30 +0900

    add I2C sample code

---
+++ nRF52_4_I2C/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "boards.h"
+#include "app_util_platform.h"
+#include "app_error.h"
+#include "nrf_drv_twi.h"
+
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
+
+
+
+int main(void)
+{
+    
+}
+
+/** @} */
