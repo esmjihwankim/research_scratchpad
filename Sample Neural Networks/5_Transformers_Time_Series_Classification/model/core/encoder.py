7aeea3c9e37e2ee4e93630704c15fa073914fd3d
Jihwan <raptorwoo@gmail.com>
2024-05-02 18:20:07 +0900

    add transformer

---
+++ Sample Neural Networks/5_Transformers_Time_Series_Classification/model/core/encoder.py
@@ -0,0 +1,20 @@
+from torch import nn
+
+from model.components.positional_encoding import PositionalEncoding
+from model.core.encoder_block import EncoderBlock
+
+class Encoder(nn.Module):
+
+    def __init__(self, d_model, ffn_hidden, n_head,
+                 n_layers, drop_prob, details, device):
+        self.layers = nn.ModuleList([EncoderBlock(d_model=d_model,
+                                                  ffn_hidden=ffn_hidden,
+                                                  n_head=n_head,
+                                                  details=details,
+                                                  drop_prob=drop_prob)
+                                     for _ in range(n_layers)])
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
